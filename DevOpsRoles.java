ca6859619ef70dd0d8bf42e73ea2a4abc8615bb8
jigneshuGit <123591992+jigneshuGit@users.noreply.github.com>
2025-08-22 09:03:00 +0530

    Create DevOpsRoles.java

---
+++ DevOpsRoles.java
@@ -0,0 +1,10 @@
+public class DevOpsRoles {
+    public static void main(String[] args) {
+        System.out.println("💻 DevOps Roles and Responsibilities:");
+        System.out.println("1. Automating infrastructure and deployments.");
+        System.out.println("2. Managing CI/CD pipelines for faster delivery.");
+        System.out.println("3. Monitoring system performance and reliability.");
+        System.out.println("4. Ensuring collaboration between Dev and Ops teams.");
+        System.out.println("5. Enhancing security, scalability, and efficiency.");
+    }
+}
